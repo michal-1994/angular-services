c00216f65224d7910ed6abe7ed1d8bf838666078
Michal <michalgrzegorczyk04@gmail.com>
2022-02-25 20:16:59 +0100

    Add Injecting

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -7,7 +7,7 @@ import { AccountsService } from '../accounts.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
 
@@ -18,6 +18,6 @@ export class NewAccountComponent {
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   }
 }
